c95b62910868d6d408227a6aea74381869401755
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-09-17 09:54:51 -0300

    Adding project of the first day of code

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,7 @@
-name = input("My name is your name?\n")
-print("Hello" + " " + name  + "!")
+print("Welcome to the city band app!")
 
+home_town = input("What is the name of your hometown?\n")
+first_pet = input("What is the name of your first pet?\n")
+band_name = home_town + " " + first_pet
+
+print("The name of your band could be called: " + band_name)
