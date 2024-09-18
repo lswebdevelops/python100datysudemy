bfd40b659d6edfb9d3dfaa16fa47c8eb04802138
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-09-18 09:49:25 -0300

    Learning type convertion

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,14 @@
-len("Hello")
+length = len("Hello")
+word = "hello"
+float1 = 223_233_333.3 # float
+number = 13123
+boolean = False
+print(type(word))
+print(type(length))
+print(type(boolean))
+print(type(float1))
+print(type(number))
+
+print(int("1223") + int("2323"))
+
+print(122 + 12)
